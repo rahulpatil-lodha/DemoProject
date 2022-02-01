9c51e078046bdd2fb4595b623b740eef5885604b
rahulpatil11031993 <rahul.patil2@lodhagroup.com>
2022-02-01 18:45:11 +0530

    add github token

--- app/src/main/java/com/rahul/newproject/MainActivity.kt
+++ app/src/main/java/com/rahul/newproject/MainActivity.kt
@@ -2,7 +2,7 @@ package com.rahul.newproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//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 -   token for githubrepo
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
