2b15f2efb16fbf7f84f989e8e4665762ae0d0419
rahulpatil11031993 <rahul.patil2@lodhagroup.com>
2022-02-02 00:09:22 +0530

    git

--- app/src/main/java/com/rahul/newproject/MainActivity.kt
+++ app/src/main/java/com/rahul/newproject/MainActivity.kt
@@ -2,7 +2,6 @@ package com.rahul.newproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//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 -   token for githubrepo
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
